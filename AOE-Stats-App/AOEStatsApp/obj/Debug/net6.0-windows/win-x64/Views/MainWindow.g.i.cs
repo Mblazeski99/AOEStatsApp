c96ea43ed2269ea7c7fcf115d5c1f8232bea3e7a
Mblazeski99 <markoblazeski1999@gmail.com>
2022-07-19 22:09:51 +0200

    f

--- AOE-Stats-App/AOEStatsApp/obj/Debug/net6.0-windows/win-x64/Views/MainWindow.g.i.cs
+++ AOE-Stats-App/AOEStatsApp/obj/Debug/net6.0-windows/win-x64/Views/MainWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace AOEStatsApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AOEStatsApp;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AOEStatsApp;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- AOE-Stats-App/AOEStatsApp/obj/Debug/net6.0-windows/win-x64/Views/UnitStatsItemListingView.g.i.cs
+++ AOE-Stats-App/AOEStatsApp/obj/Debug/net6.0-windows/win-x64/Views/UnitStatsItemListingView.g.i.cs
@@ -134,7 +134,7 @@ namespace AOEStatsApp.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AOEStatsApp;V1.0.0.0;component/views/unitstatsitemlistingview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AOEStatsApp;component/views/unitstatsitemlistingview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\UnitStatsItemListingView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
