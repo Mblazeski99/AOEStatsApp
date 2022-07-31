2f20a311d4b9b71247ffdd3fcbf49f44d9a20209
Mblazeski99 <markoblazeski1999@gmail.com>
2022-07-31 03:18:11 +0200

    added placeholder textbox custom control

---
+++ AOE-Stats-App/AOEStatsApp/CustomControls/PlaceholderTextBox/PlaceholderTextBox.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AOEStatsApp.CustomControls
+{
+    public class PlaceholderTextBox : TextBox
+    {
+        public string Placeholder
+        {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceholderProperty =
+            DependencyProperty.Register(nameof(Placeholder), typeof(string), typeof(PlaceholderTextBox), new PropertyMetadata(string.Empty));
+
+        public bool IsEmpty
+        {
+            get { return (bool)GetValue(IsEmptyProperty); }
+            private set { SetValue(IsEmptyPropertyKey, value); }
+        }
+
+        private static readonly DependencyPropertyKey IsEmptyPropertyKey =
+            DependencyProperty.RegisterReadOnly(nameof(IsEmpty), typeof(bool), typeof(PlaceholderTextBox), new PropertyMetadata(true));
+
+        private static readonly DependencyProperty IsEmptyProperty = IsEmptyPropertyKey.DependencyProperty;
+
+        static PlaceholderTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlaceholderTextBox), new FrameworkPropertyMetadata(typeof(PlaceholderTextBox)));
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            IsEmpty = string.IsNullOrEmpty(Text);
+            base.OnTextChanged(e);
+        }
+    }
+}
